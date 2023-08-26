e3dd9efa1650321899e09470e497740d7920a12a
Adarsh <adarshcode984@gmail.com>
2023-08-26 10:28:36 +0530

    I connected firebase to app

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjsTEXK77i1WZ1umLFQ474E4vpZH7nxWs",
+  authDomain: "web-c1e10.firebaseapp.com",
+  projectId: "web-c1e10",
+  storageBucket: "web-c1e10.appspot.com",
+  messagingSenderId: "629113868499",
+  appId: "1:629113868499:web:b72b35256a90d038a00426"
+};
+
+const app = initializeApp(firebaseConfig);
